1271e113cd38c7b4902704b9af7d2b6a138faaf8
RanimAmor <ranim.amor@medtech.tn>
2021-08-06 11:27:43 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
+import './style.css';
 import './App.css';
-
+import img from "./imageInSrc.jpg"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div style={{border:"solid 1px black",maxWidth:"100vw"}}>
+    <h1 className="title red">Ranim Amor</h1>
+    <br></br>
+    <div style={{textAlign:"center"}} >
+    <img src={img}></img>
+    <br></br>   <br></br>
+    <img src="/imageInPublic.jpg"></img>
     </div>
+    </div>
+    <div  style={{textAlign:"center"}}>
+    <video width="320" height="240"controls>
+    <source src="vid.mp4" type="video/mp4" ></source>
+    </video>
+    </div>
+    </>
   );
 }
 
